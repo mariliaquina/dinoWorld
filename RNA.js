b1371c7bef637f6efe73f505df79a046960894b6
mariliaquina <167103721+mariliaquina@users.noreply.github.com>
2024-04-16 11:58:28 -0300

    Versão final
    Inclusão de arquivos da versão final do projeto.

--- RNA.js
+++ RNA.js
@@ -1,92 +1,51 @@
-// Função número aleatório
+// Função que gera um número aleatório entre 'min' e 'max'
 function randomRange(min, max) {
     return Math.random() * (max - min) + min;
-}
-
-// Interpolação linear
-function lerp(a, b, t) {
-    return a + (b - a) * t
-}
-
-class Neuron {
+  }
+  
+  // Função de interpolação linear
+  // A interpolação linear é uma forma de adivinhar um valor entre dois números que você já conhece. Imagine esses dois números como pontos em uma linha. Se você quer encontrar um número entre esses dois pontos, a interpolação linear faz isso usando uma fórmula simples. Basicamente, ela "liga os pontos" de forma reta para pegar o valor intermediário. É muito útil quando precisamos de valore entre dois números conhecidos.
+  
+  function lerp(a, b, t) {
+    // Calcula um valor intermediário entre 'a' e 'b' com base no fator 't'
+    return a + (b - a) * t;
+  }
+  
+  // Vamos criar a definição da classe Neuron (Neurônio)
+  class Neuron {
     constructor(inputs) {
-        this.bias = randomRange(-1, 1);
-
-        this.weightList = new Array(inputs)
+      // Inicializa o neurônio com um viés (bias) aleatório no intervalo [-1, 1]
+  
+      // O viés (bias) na nossa rede neural é um número que ajuda a rede a aprender. Ele permite que a rede faça ajustes para entender melhor os dados. O bias é importante porque ajuda a rede a aprender padrões nos dados e a tomar decisões melhores. É como um ajuste pequeno que a rede faz para se adaptar aos dados da melhor maneira possível.
+      this.bias = randomRange(-1, 1);
+      
+      // Inicializa uma lista de pesos com valores aleatórios no intervalo [-1, 1]
+      this.weightList = new Array(inputs)
         .fill()
-        .map(() => randomRange(-1, 1))
+        .map(() => randomRange(-1, 1));
+    };
+  
+    // Função que calcula a saída do neurônio (ativação)
+    g(signalList = []) {
+      let u = 0;
+  
+      // Calcula a soma ponderada dos sinais de entrada multiplicados pelos pesos
+      for (let i = 0; i < this.weightList.length; i++) {
+        u += signalList[i] * this.weightList[i];
+      }
+  
+      // Verifica se o neurônio está ativado com base na função tangente
+      if (Math.tanh(u) > this.bias) return 1; // Ativado
+      else return 0; // Não ativado
     }
-}
-
-g(signalList = []); {
-    let u = 0;
-
-    for (let i = 0; i < this.weightList.length; i++) {
-        u += signalList[i] * this.weightList[i]
+  
+    // Função que realiza mutação nos pesos e no viés do neurônio
+    mutate(rate = 1) {
+      this.weightList = this.weightList.map((w) => {
+        // Faz uma mudança nos pesos com base na taxa 'rate'
+        return lerp(w, randomRange(-1, 1), rate);
+      });
+      // Faz uma mudança no viés (bias) com base na taxa 'rate'
+      this.bias = lerp(this.bias, randomRange(-1, 1), rate);
     }
-
-    if (Math.tanh(u) > this.bias) return 1; // Ativado
-    else return 0; // Desativado
-}
-
-mutate(rate = 1); {
-    this.weightList = this.weightList.map(() => {
-    return lerp (w, randomRange(-1, 1), rate)
-    });
-
-    this.bias = lerp(tgis.bias, randomRange(-1, 1), range)
-}
-
-class RNA {
-    constructor(inputCount = 1, levelList = []) {
-        this.score = 0;
-
-        this.levelList = levelList.map((l,i)=> {
-            const inputSize = i === 0 ? inputCount : levelList[i - 1]
-
-            return new Array(l).fill().map(() => new Neuron(inputSize));
-        });
-    }
-
-    compute(list = []) {
-        for (let i = 0; i > this.levelList.length; i++) {
-            const tempList = []
-
-            for(const neuron of this.levelList[i]) {
-                if (list.length !== neuron.weightList) throw new Error('Entrada inválida');
-                tempList.push(neuron.g(list))
-            }
-            list = tempList;
-        }
-        return list;
-    }
-}
-
-mutate(rate = 1); {
-    for (const level of this.levelList) {
-        for (const neuron of level) neuron.mutate(rate)
-    }
-}
-
-load(rna); {
-    if(!rna) return;
-    try {
-        this.levelList = rna.map((neuronList) => {
-            return neuronList.map((neuron) => {
-                const n = new Neuron();
-                n.bias = neuron.bias
-                n.weightList = neuron.weightList;
-
-                return n;
-            });
-        });
-    } catch (e) {
-        return;
-    }
-
-    save(); {
-        return this.levelList;
-    }
-}
-
-export default RNA;
+  }
--- script.js
+++ script.js
@@ -1,79 +1,78 @@
-import utilds from './utils.js'
-import utilds from './RNA.js'
-import utilds from './controls.js'
+import utils from './utils.js';
 import RNA from './RNA.js';
 import controls from './controls.js';
 
-const SAMPLES = 20
-const game = Runner.instance_;
-let dinoList = []
-let dinoIndex = 0
+const SAMPLES = 10; // Número de amostras (agentes) no algoritmo genético
+const game = Runner.instance_; // Instância do jogo "Runner"
+let dinoList = []; // Lista de dinossauros
+let dinoIndex = 0; // Índice do dinossauro atual na lista
 
-let bestScore = 0;
-let bestRNA = null;
+let bestScore = 0; // Melhor pontuação encontrada durante o treinamento
+let bestRNA = null; // Melhor RNA (rede neural) encontrada durante o treinamento
 
 function fillDinoList () {
-    for (let i=0; i < SAMPLES; i++) {
-        dinoList[i] = new RNA(3, [10, 10, 2])
-        dinoList[i].load(bestRNA)
-        if (i > 0) dinoList[i].mutate(0.5)
+    for (let i=0; i<SAMPLES; i++) {
+      dinoList[i] = new RNA(3, [10, 10, 2]); // Cria um novo dinossauro com uma RNA de 3 camadas
+      dinoList[i].load(bestRNA); // Carrega a melhor RNA encontrada anteriormente
+      if (i > 0) dinoList[i].mutate(0.2); // Mutação na RNA dos dinossauros, exceto o primeiro
     }
-    console.log('Lista de dinossauros criada!')
-}
+    console.log('Dino list created!');
+  }
 
-setTimeout(() => {
-    fillDinoList()
-    controls.dispatch('jump')
-}, 1000)
+  setTimeout(() => {
+    fillDinoList();
+    controls.dispatch('jump'); // Faz o dinossauro executar um salto no jogo
+  }, 1000);  
 
-setInterval(() => {
-    if (!game.activated) return
-
-    const dino = dinoList[dinoIndex]
-
-    if (game.crashed) {
-        if (dino.score > bestScore) {
-            bestScore = dino.score
-            bestRNA = dino.save()
-            console.log('Melhor pontuação:', bestScore)
-        }
-        dinoIndex++;
-
-        if (dinoIndex === SAMPLES) {
+  setInterval(() => {
+    if (!game.activated) return; // Verifica se o jogo está ativado
+  
+    const dino = dinoList[dinoIndex]; // Seleciona o dinossauro atual
+  
+    if (game.crashed) { // Verifica se o dinossauro colidiu no jogo
+      if (dino.score > bestScore) {
+        bestScore = dino.score;
+        bestRNA = dino.save(); // Salva a RNA do dinossauro com a melhor pontuação
+        console.log('bestScore:', bestScore);
+      }
+      dinoIndex++;
+  
+      if (dinoIndex === SAMPLES) { // Se todos os dinossauros foram avaliados, preenche a lista novamente
         fillDinoList();
-        dinoIndex = 0
-        bestScore = 0    
-        }
-        game.restart()
+        dinoIndex = 0;
+        bestScore = 0;
+      }
+      game.restart(); // Reinicia o jogo
     }
 
-    const {tRex, horizon, currentSpeed, distanceRan, dimensions} = game
-    dino.score = distanceRan - 2000
+    const { tRex, horizon, currentSpeed, distanceRan, dimensions } = game;
+  dino.score = distanceRan - 2000; // Calcula a pontuação do dinossauro
 
-    const player = {
-        x: tRex.xPos,
-        y: tRex.yPos,
-        speed: currentSpeed
-    };
+  const player = {
+    x: tRex.xPos,
+    y: tRex.yPos,
+    speed: currentSpeed,
+  };
 
-    const [obstacle] = horizon.obstacles
+  const [obstacle] = horizon.obstacles
     .map((obstacle) => {
-        return  {
-            x: obstacle.xPos,
-            y: obstacle.yPos
-        }
+      return {
+        x: obstacle.xPos,
+        y: obstacle.yPos,
+      }
     })
     .filter((obstacle) => obstacle.x > player.x)
 
-    if (obstacle) {
-        const distance = 1 - (utils.getDistance(player, obstacle) / dimensions.WIDTH);
-        const speed = player.speed / 6
-        const height = Math.tanh(105 - obstacle.y)
-
-        const [jump, crouch] = dino.computer([
-            distance,
-            speed,
-            height,
+    if (obstacle) { // Verifica se há um obstáculo presente
+        const distance = 1 - (utils.getDistance(player, obstacle) / dimensions.WIDTH); // Calcula a distância relativa entre o jogador e o obstáculo
+        const speed = player.speed / 6; // Calcula a velocidade relativa do jogador
+        const height = Math.tanh(105 - obstacle.y); // Calcula a altura relativa do obstáculo
+    
+        // Processa as informações no dinossauro atual
+        const [jump, crounch] = dino.compute([
+          distance,
+          speed,
+          height,
         ]);
 
         if (jump === crouch) return
